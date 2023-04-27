a3bf9603eaf494183139165004cf5eb563ab2c1f
Ana Qonitah Munawwaroh <75164626+anaqonitah27@users.noreply.github.com>
2023-04-27 19:38:08 +0700

    Logout

--- src/app/admin/logout/logout.component.ts
+++ src/app/admin/logout/logout.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-logout',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./logout.component.css']
 })
 export class LogoutComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
 }
