5a98ad4662b74e37c95859d7653e3810c2ff6b51
Ana Qonitah Munawwaroh <75164626+anaqonitah27@users.noreply.github.com>
2023-04-30 00:13:32 +0700

    Home Progress

--- src/app/admin/home/home.component.ts
+++ src/app/admin/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +10,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
 }
